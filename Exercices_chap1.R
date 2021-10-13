b8f2dd5bf3f150ba99f353200b68d65f1d647f9c
Ibrahima Sy <syibrahima31@goutlook.com>
2021-10-13 20:29:52 +0000

    exo

---
+++ Exercices_chap1.R
@@ -0,0 +1,29 @@
+# {
+#   a <-  0
+#   a <-  a + 30
+#   a
+# }
+#
+v <- c(1,2,3,4)
+
+mode(v)
+
+length(v)
+
+class(v)
+
+typeof(v)
+
+
+a <-  TRUE
+
+
+numeric(10)
+
+logical(10)
+
+
+z <-  1 + 2i
+
+
+chaine <-  "je suis un actuaire "
